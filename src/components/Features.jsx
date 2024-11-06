88f2d3c916a337fa8d1ac093662acd8f0b38d459
Siva12443 <Siva12443@outlook.Com>
2024-11-06 13:16:02 +0530

    all error solved

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -184,33 +184,7 @@ function Features() {
         </div>
       </div>
 
-      {/* Bottom Image */}
-      <div className="flex items-center ">
-        <div className="w-1/2 items-center justify-center flex flex-col">
-        <div className="bg-[#F7F2E7] w-[10%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[20%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[30%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[40%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[50%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[60%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[70%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[80%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[90%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[100%] h-[3px] md:h-[9px] "></div>
-        </div>
-        <div className="w-1/2 items-center justify-center flex flex-col">
-        <div className="bg-[#F7F2E7] w-[10%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[20%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[30%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[40%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[50%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[60%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[70%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[80%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[90%] h-[3px] md:h-[9px] "></div>
-        <div className="bg-[#F7F2E7] w-[100%] h-[3px] md:h-[9px] "></div>
-        </div>
-      </div>
+      
       {/* <img src={btm} alt="" className="w-full" /> */}
     </div>
   );
