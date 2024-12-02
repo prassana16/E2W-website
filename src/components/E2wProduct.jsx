8b7903b3216d9b180949f97394a5cf56435e53b8
Asath1304 <asathkameel131@gmail.com>
2024-12-02 17:58:05 +0530

    e2w products ui changes

--- src/components/E2wProduct.jsx
+++ src/components/E2wProduct.jsx
@@ -1,52 +1,60 @@
 "use client";
 import React from "react";
 import { StickyScroll } from "./ui/sticky-scroll-reveal";
-import mobileMockup from '../assets/images/ModuleImages/e2wAbout.png';
-import e2waboutphn from '../assets/images/ModuleImages/e2waboutphn.png';
-import clientManagerImage from '../assets/images/ModuleImages/e2wClient.png';
-import e2wclientphn from '../assets/images/ModuleImages/e2wclientphn.png';
-import consultantImage from '../assets/images/ModuleImages/e2wCounsultant.png';
-import e2wconsultantmanagerphn from '../assets/images/ModuleImages/e2wconsultantmanagerphn.png';
-import FinanceManagerImage from '../assets/images/ModuleImages/e2wFinance.png';
-import e2wfinancephn from '../assets/images/ModuleImages/e2wfinancephn.png';
-import FinanceReportImage from '../assets/images/ModuleImages/e2wFinanceReport.png';
-import e2wfinancereportphn from '../assets/images/ModuleImages/e2wfinancereportphn.png';
-import OrderManagerImage from '../assets/images/ModuleImages/e2wOrder.png';
-import e2worderphn from '../assets/images/ModuleImages/e2worderphn.png';
-import OrderReportImage from '../assets/images/ModuleImages/e2wOrderReport.png';
-import e2worderreportphn from '../assets/images/ModuleImages/e2worderreportphn.png';
-import PaymentMilestoneImage from '../assets/images/ModuleImages/paymentmilestone.png';
-import e2wpaymilephn from '../assets/images/ModuleImages/e2wpaymilephn.png';
-import quoteSenderImage from '../assets/images/ModuleImages/e2wQuote.png';
-import e2wquotephn from '../assets/images/ModuleImages/e2wquotephn.png';
-import rateManagerImage from '../assets/images/ModuleImages/e2wRatemanager.png';
-import e2wratesphn from '../assets/images/ModuleImages/e2wratesphn.png';
-import RateValidationImage from '../assets/images/ModuleImages/e2wRatevalid.png';
-import e2wratevalidationphn from '../assets/images/ModuleImages/e2wratevalidationphn.png';
-import userManagerImage from '../assets/images/ModuleImages/usermanager.png';
-import e2wusermanagerphn from '../assets/images/ModuleImages/e2wusermanagerphn.png';
-import appointmentManagerImage from '../assets/images/ModuleImages/appointmentmanager.png';
-import e2wappointmentphn from '../assets/images/ModuleImages/e2wappointmentphn.png';
-import conclusionImage from '../assets/images/ModuleImages/conclusion.png';
-import e2wconclusionphn from '../assets/images/ModuleImages/e2wconclusionphn.png';
+import e2wlogin from '../assets/images/e2w-products-laptop/e2wlogout.png';
+import e2wclient from '../assets/images/e2w-products-laptop/e2wclient.png';
+import e2wquote from '../assets/images/e2w-products-laptop/e2wquote.png';
+import e2worder from '../assets/images/e2w-products-laptop/e2worder.png';
+import e2wfinance from '../assets/images/e2w-products-laptop/e2wfinance.png';
+import e2wratesmanager from '../assets/images/e2w-products-laptop/e2wratesmanager.png';
+import e2wratesvalidation from '../assets/images/e2w-products-laptop/e2wratesvalidation.png';
+import e2worderreport from '../assets/images/e2w-products-laptop/e2worderreport.png';
+import e2wfinancereport from '../assets/images/e2w-products-laptop/e2wfinancereport.png';
+import e2wconsultantreport from '../assets/images/e2w-products-laptop/e2wconsultantreport.png';
+import e2wusermanager from '../assets/images/e2w-products-laptop/e2wusermanager.png';
+import e2wconsultantmanager from '../assets/images/e2w-products-laptop/e2wconsultantmanager.png';
+import e2wappointment from '../assets/images/e2w-products-laptop/e2wappointment.png';
+import e2wpayment from '../assets/images/e2w-products-laptop/e2wpayment.png';
+import e2wlogout from '../assets/images/e2w-products-laptop/e2wlogout.png';
+//phn images
+import e2wloginphn from '../assets/images/e2w-mobile-img/e2wloginphn.png';
+import e2wclientphn from '../assets/images/e2w-mobile-img/e2wclientphn.png';
+import e2wquotephn from '../assets/images/e2w-mobile-img/e2wquotephn.png';
+import e2worderphn from '../assets/images/e2w-mobile-img/e2worderphn.png';
+import e2wfinancephn from '../assets/images/e2w-mobile-img/e2wfinancephn.png';
+import e2wratesmanagerphn from '../assets/images/e2w-mobile-img/e2wratesmanagerphn.png';
+import e2wratesvalidationphn from '../assets/images/e2w-mobile-img/e2wratesvalidationphn.png';
+import e2worderreportphn from '../assets/images/e2w-mobile-img/e2worderreportphn.png';
+import e2wfinancereportphn from '../assets/images/e2w-mobile-img/e2wfinancereportphn.png';
+import e2wconsultantreportphn from '../assets/images/e2w-mobile-img/e2wconsultantreportphn.png';
+import e2wusermanagerphn from '../assets/images/e2w-mobile-img/e2wusermanagerphn.png';
+import e2wconsultantmanagerphn from '../assets/images/e2w-mobile-img/e2wconsultantmanagerphn.png';
+import e2wappointmentphn from '../assets/images/e2w-mobile-img/e2wappointmentphn.png';
+import e2wpaymentphn from '../assets/images/e2w-mobile-img/e2wpaymentphn.png';
+import e2wlogoutphn from '../assets/images/e2w-mobile-img/e2wlogoutphn.png';
+
+
 
 const content = [
   {
     title: "About Easy2Work",
-    description:
-      "Easy2Work is an all-in-one workflow management system designed to optimize business processes and streamline daily operations. Customization is our value-add. It provides an intuitive interface for managing clients, handling rate cards, processing orders, and governing financial transactions.",
+    description: [
+      <li>Customizable workflows.</li>,
+      <li>Manage clients and orders.</li>,
+      <li>User-friendly design.</li>,
+    ],
     content: (
-        <div className="h-full w-full  flex items-center justify-center text-white">
-         {/* Desktop View Image */}
+        <div className="h-full w-full flex flex-col sm:flex-row items-center justify-center text-white">
+         {/* Desktop View Image */}             
       <img
-        src={mobileMockup}
+        src={e2wlogin}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
       
       {/* Phone View Image */}
       <img
-        src={e2waboutphn}
+        src={e2wloginphn}
         className="block sm:hidden h-auto max-w-full object-contain"
         alt="linear board demo - phone"
       />
@@ -55,13 +63,15 @@ const content = [
   },
   {
     title: "Client Manager",
-    description:
-      "Our Client Manager helps businesses manage client data efficiently with lead source capture, easy client search, and secure cloud storage.",
+    description: [
+      <li>Add client details.</li>,
+      <li>Secure cloud storage.</li>,
+      <li>Quick client search.</li>,    ],
     content: (
       <div className="h-full w-full  flex items-center justify-center text-white">
         {/* Desktop View Image */}
       <img
-        src={clientManagerImage}
+        src={e2wclient}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
@@ -78,13 +88,16 @@ const content = [
   
   {
     title: "Quote Sender",
-    description:
-      "The Quote Sender tool simplifies the process of creating and sending quotes with dynamic margin adjustments, slab-wise pricing, and the ability to download quotes seamlessly.",
+    description: [
+      <li>Send quotes easily.</li>,
+      <li>Slab-wise pricing.</li>,
+      <li>Adjust margins quickly.</li>,
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
        {/* Desktop View Image */}
       <img
-        src={quoteSenderImage}
+        src={e2wquote}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
@@ -100,13 +113,16 @@ const content = [
   },
   {
     title: "Order Manager",
-    description:
-      "The Order Manager simplifies the way businesses handle orders, from tracking lead sources to organizing client information with fast search capabilities and secure cloud storage.",
+    description: [
+      <li>Track orders.</li>,
+      <li>Organize clients.</li>,
+      <li>Fast and secure search.</li>,
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
          {/* Desktop View Image */}
       <img
-        src={OrderManagerImage}
+        src={e2worder}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
@@ -122,13 +138,16 @@ const content = [
   },
   {
     title: "Finance Manager",
-    description:
-      "The Finance Manager empowers businesses to efficiently manage client data with features like customized lead source tracking, quick client search, and secure cloud storage for peace of mind.",
+    description: [
+      <li>Track revenue and expenses.</li>,
+      <li>Real-time insights.</li>,
+      <li>Monitor performance.</li>,
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
          {/* Desktop View Image */}
       <img
-        src={FinanceManagerImage}
+        src={e2wfinance}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
@@ -144,20 +163,23 @@ const content = [
   },
   {
     title: "Rate Manager",
-    description:
-      "The Rate Manager module is designed to simplify rate management with its user-friendly interface. It allows businesses to search rates, set slab-wise pricing, and define service duration for different campaigns or products with ease.",
+    description: [
+      <li>Simplify rate management.</li>,
+      <li>Add slab-wise pricing.</li>,
+      <li>Define service duration.</li>,
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
          {/* Desktop View Image */}
       <img
-        src={rateManagerImage}
+        src={e2wratesmanager}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
       
       {/* Phone View Image */}
       <img
-        src={e2wratesphn}
+        src={e2wratesmanagerphn}
         className="block sm:hidden h-auto max-w-full object-contain"
         alt="linear board demo - phone"
       />
@@ -166,20 +188,23 @@ const content = [
   },
   {
     title: "Rate Validation",
-    description:
-      "The Rate Validation feature ensures that your pricing is consistent, competitive, and accurate across all platforms, helping businesses maintain financial accuracy and compliance.",
+    description: [
+      <li>Ensure pricing consistency.</li>,
+      <li>Maintain accuracy.</li>,
+      <li>Avoid errors.</li>,
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
         {/* Desktop View Image */}
       <img
-        src={RateValidationImage}
+        src={e2wratesvalidation}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
       
       {/* Phone View Image */}
       <img
-        src={e2wratevalidationphn}
+        src={e2wratesvalidationphn}
         className="block sm:hidden h-auto max-w-full object-contain"
         alt="linear board demo - phone"
       />
@@ -188,13 +213,16 @@ const content = [
   },
   {
     title: "Order Report",
-    description:
-      "The Order Report provides businesses with detailed insights into order tracking, fulfillment, and performance analysis, helping to streamline operations and optimize customer satisfaction.",
+    description: [
+      <li>Track order progress.</li>,
+      <li>Optimize processes.</li>,
+      <li>Enhance satisfaction.</li>,
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
        {/* Desktop View Image */}
       <img
-        src={OrderReportImage}
+        src={e2worderreport}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
@@ -210,13 +238,16 @@ const content = [
   },
   {
     title: "Finance Report",
-    description:
-      "The Finance Report feature provides businesses with real-time financial insights, offering detailed breakdowns of revenue, expenses, and performance metrics to ensure efficient financial management.",
+    description: [
+      <li>View revenue breakdown.</li>,
+      <li>Track expenses.</li>,
+      <li>Analyze metrics.</li>
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
         {/* Desktop View Image */}
       <img
-        src={FinanceReportImage}
+        src={e2wfinancereport}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
@@ -231,14 +262,43 @@ const content = [
     ),
   },
   {
-    title: "User Manager",
-    description:
-      "The User Manager empowers businesses to oversee employee access, manage roles, and ensure secure usage of the software. Designed for seamless employee management, it provides tools for role assignment, activity tracking, and user permissions.",
+    title: "Consultant Report",
+    description: [
+      <li>Track performance.</li>,
+      <li>Analyze productivity.</li>,
+      <li>View contributions.</li>,
+    ],
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         {/* Desktop View Image */}
         <img
-          src={userManagerImage}
+          src={e2wconsultantreport}
+          className="hidden sm:block h-auto max-w-full object-contain"
+          alt="consultant report demo"
+        />
+  
+        {/* Phone View Image */}
+        <img
+          src={e2wconsultantreportphn}
+          className="block sm:hidden h-auto max-w-full object-contain"
+          alt="consultant report demo - phone"
+        />
+      </div>
+    ),
+  },
+  
+  {
+    title: "User Manager",
+    description: [
+      <li>Manage roles and access.</li>,
+      <li>Track user activity.</li>,
+      <li>Ensure security.</li>,
+    ],
+    content: (
+      <div className="h-full w-full flex items-center justify-center text-white">
+        {/* Desktop View Image */}
+        <img
+          src={e2wusermanager}
           className="hidden sm:block h-auto max-w-full object-contain"
           alt="user manager demo"
         />
@@ -254,13 +314,16 @@ const content = [
   },
   {
     title: "Consultant Manager",
-    description:
-      "The Consultant Manager enables businesses to efficiently manage consultant engagements, track performance, and streamline communication for better collaboration and results.",
+    description: [
+      <li>Manage consultants.</li>,
+      <li>Track performance.</li>,
+      <li>Enhance collaboration.</li>,
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
          {/* Desktop View Image */}
       <img
-        src={consultantImage}
+        src={e2wconsultantmanager}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
@@ -276,13 +339,16 @@ const content = [
   },
   {
     title: "Appointment Manager",
-    description:
-      "The Appointment Manager streamlines scheduling by allowing businesses to send appointment invites to clients effortlessly. It enhances coordination, reduces conflicts, and ensures timely follow-ups for improved client relations.",
+    description: [
+      <li>Schedule appointments.</li>,
+      <li>Send invites.</li>,
+      <li>Follow up with clients.</li>,
+    ],  
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         {/* Desktop View Image */}
         <img
-          src={appointmentManagerImage}
+          src={e2wappointment}
           className="hidden sm:block h-auto max-w-full object-contain"
           alt="appointment manager demo"
         />
@@ -298,20 +364,23 @@ const content = [
   },  
   {
     title: "Payment Milestones",
-    description:
-      "Our Payment Manager offers a streamlined approach to track Payment milestones, ensuring timely delivery and effective communication across teams.",
+    description: [
+      <li>Track milestones.</li>,
+      <li>Improve communication.</li>,
+      <li>Meet delivery timelines.</li>,
+    ],
     content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
          {/* Desktop View Image */}
       <img
-        src={PaymentMilestoneImage}
+        src={e2wpayment}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
       
       {/* Phone View Image */}
       <img
-        src={e2wpaymilephn}
+        src={e2wpaymentphn}
         className="block sm:hidden h-auto max-w-full object-contain"
         alt="linear board demo - phone"
       />
@@ -320,20 +389,23 @@ const content = [
   },
   {
     title: "Conclusion",
-    description:
-      "EasyToWork Workflow Management Software is designed to simplify and enhance business operations, offering customizable solutions that drive efficiency, improve team communication, and ensure timely project milestones. With EasyToWork, businesses can manage workflows seamlessly, ensuring all tasks, payments, and communications are optimized for maximum productivity.",
+    description: [
+      <li>Simplify operations.</li>,
+      <li>Boost efficiency.</li>,
+      <li>Maximize productivity.</li>,
+    ],
       content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
          {/* Desktop View Image */}
       <img
-        src={conclusionImage}
+        src={e2wlogout}
         className="hidden sm:block h-auto max-w-full object-contain"
         alt="linear board demo"
       />
       
       {/* Phone View Image */}
       <img
-        src={e2wconclusionphn}
+        src={e2wlogoutphn}
         className="block sm:hidden h-auto max-w-full object-contain"
         alt="linear board demo - phone"
       />
@@ -349,7 +421,7 @@ export function E2wProduct() {
       >
         Our Workflow Management Tools.
       </h2>
-    <div className="h-[100vh]">
+    <div className="h-[50vh] md:h-[100vh] ">
       <StickyScroll content={content} />
     </div>
     </div>
