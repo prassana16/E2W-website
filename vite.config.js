c0559f807ab0a11fb9379fe91a721635fbcf0ea7
siva-12443 <siva12443@outlook.com>
2025-06-05 14:07:22 +0530

    E2W-website/ loads by defeault issuse is fixed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/E2W-website/',
+  base: '/',
   server: {
     host: true
   },
