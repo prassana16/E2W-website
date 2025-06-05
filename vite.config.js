57caf04773d44f191bfa172cc390e86ad60e29dd
Saravanan kiruba S <kirubachetti@gmail.com>
2025-06-05 12:49:27 +0530

    h

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/E2W-website/',
   server: {
     host: true
   }
